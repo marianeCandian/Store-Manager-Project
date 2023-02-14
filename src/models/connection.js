8dba03dd7a9411c12fcc9c75055c8d5245039fe4
Mariane Candian <mariane.candian@gmail.com>
2023-02-14 20:09:58 -0300

    testando correção do connection-2

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
